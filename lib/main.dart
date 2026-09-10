87b77f7ef8604cb4b02667d65eba39da8396df95
lithium <lithium091198@gmail.com>
2026-09-10 01:41:28 -0400

    Update import path for main_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'screens/main_screen.dart';
+import 'package:mindmod/screens/main_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
