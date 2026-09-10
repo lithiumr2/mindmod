32a0cd8c00c1d77361cf7d76de2f6ed67bbaa8fc
lithium <lithium091198@gmail.com>
2026-09-10 15:43:10 -0400

    Refactor ProjectFile class structure and add JSON methods
    Refactor ProjectFile class to use final fields and add JSON serialization methods.

--- lib/models/project_file.dart
+++ lib/models/project_file.dart
@@ -1,19 +1,37 @@
-import 'dart:typed_data';
-
-enum FileType { hjson, image, json }
+enum FileType { json, hjson, image }
 
 class ProjectFile {
-  String name;
-  String content;
-  Uint8List? binaryContent;
-  FileType type;
+  final String name;
+  final FileType type;
+  final String content;
 
   ProjectFile({
     required this.name,
-    this.content = '',
-    this.binaryContent,
     required this.type,
+    required this.content,
   });
 
   bool get isImage => type == FileType.image;
+
+  ProjectFile copyWith({String? name, FileType? type, String? content}) {
+    return ProjectFile(
+      name: name ?? this.name,
+      type: type ?? this.type,
+      content: content ?? this.content,
+    );
+  }
+
+  // Convierte el objeto a JSON para guardarlo
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'type': type.index,
+        'content': content,
+      };
+
+  // Crea el objeto desde el JSON guardado
+  factory ProjectFile.fromJson(Map<String, dynamic> json) => ProjectFile(
+        name: json['name'],
+        type: FileType.values[json['type']],
+        content: json['content'],
+      );
 }
