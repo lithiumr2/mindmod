90718217dd28f35f040a5f16db7c4fe116a75c80
lithium <lithium091198@gmail.com>
2026-09-16 11:58:20 -0400

    fix: syntax error in common_form_helpers and DrawerBuilderWidget refresh state

--- lib/widgets/visual_form_widget.dart
+++ lib/widgets/visual_form_widget.dart
@@ -722,6 +722,7 @@ class _VisualFormWidgetState extends ConsumerState<VisualFormWidget> {
                       SizedBox(
                         width: constraints.maxWidth,
                         child: DrawerBuilderWidget(
+                          key: ValueKey(activeFile.path),
                           blockProperties: _properties,
                           availableLiquids: _getAllAvailableLiquids(),
                           onChanged: (newDrawer) {
